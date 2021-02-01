f9f6aa7550da81a67136976b8f8e87aae0f9c06b
RenanPimentel <renansinhorini12@gmail.com>
2021-02-01 18:13:07 -0300

    adapted to cellphone

--- src/Navigator.jsx
+++ src/Navigator.jsx
@@ -5,12 +5,16 @@ import NavigatorItem from './NavigatorItem';
 function Navigator() {
   const items = [
     { name: 'Items', href: '#items' },
+    { name: 'Discord', href: '#discord' },
     { name: 'About us', href: '#about-us' },
-    { name: 'Discord', href: '#discord' }
   ];
 
   return (
     <nav className="navigator">
+      <div className="input-container">
+        <input type="text"/>
+        <button>🔎</button>
+      </div>
       {items.map((item, index) => <NavigatorItem {...item} key={index}/>)}
     </nav>
   );
